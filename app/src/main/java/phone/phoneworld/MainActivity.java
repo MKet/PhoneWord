ffe0c73de4e395dbb50865548415550d7e931ac5
Marco Ketelaars <marco-ketelaars94@hotmail.com>
2018-02-08 23:48:32 +0100

    Get reference to Ui objects in activity

--- app/src/main/java/phone/phoneworld/MainActivity.java
+++ app/src/main/java/phone/phoneworld/MainActivity.java
@@ -1,13 +1,37 @@
 package phone.phoneworld;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+
+import logic.PhoneNumberTranslator;
 
 public class MainActivity extends AppCompatActivity {
+    private static final ArrayList<String> phoneNumbers = new ArrayList<>();
+    private static final int REQUEST_PHONE_CALL = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        EditText numberText = findViewById(R.id.PhoneNumberText);
+        Button callButton = findViewById(R.id.CallButton);
+        Button translateButton = findViewById(R.id.TranslateButton);
+        Button callHistoryButton = findViewById(R.id.HistoyButton);
+
+        callButton.setEnabled(false);
+
+
     }
 }
